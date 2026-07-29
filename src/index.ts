224be347fde0b55fba1675b912a83b9338b860fb
William Liebenberg [Microsoft MVP] <awliebenberg@outlook.com>
2026-07-29 15:59:13 +1000

    refactor: remove unused functions and update module settings in TypeScript configuration

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,3 @@ import { configureTelemetry } from "./shared/telemetry";
 configureTelemetry();
 
 import "./functions/httpOrders";
-import "./functions/processCoffeeOrders";
-import "./functions/receiptBlobTrigger";
-import "./functions/orderChangeFeed";
